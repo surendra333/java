d0d9f3b8024ad003f22efb8e29c41f52c7a7f573
Surender <Surender@DESKTOP-1N6J5DC>
2021-02-12 16:40:00 +0530

    d

---
+++ src/MyFirstProject/sss.java
@@ -0,0 +1,13 @@
+package MyFirstProject;
+
+public class sss {
+
+	public static void main(String[] args) {
+		int age=26;
+	    int weight=51;
+		if(age>=25) 
+		if(weight>50) {
+			System.out.println("selected");
+		}
+		}
+	}
